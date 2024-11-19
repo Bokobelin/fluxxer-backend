d29de82faa4cac50b53b29e25b3f4c5f6f11258a
Bokobelin <164776021+Bokobelin@users.noreply.github.com>
2024-11-19 13:19:12 +0100

    Update posts.js

--- api/posts.js
+++ api/posts.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 // MongoDB connection setup
 const connectToDB = async () => {
-  await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+  await mongoose.connect(process.env.MONGO_URI);
 };
 
 // Post schema and model
